9d1de6467d8746dbd2258c604c549b3cf2252e52
Destino Dello <destinodello@gmail.com>
2021-12-07 03:30:50 +0000

    modified homeBanner

--- components/homepage/HomeBanner.js
+++ components/homepage/HomeBanner.js
@@ -8,12 +8,12 @@ export default function HomeBanner() {
         className="font-size-display1 mt-5 text-center mx-auto text-uppercase"
         style={{ maxWidth: '53rem' }}
       >
-        Moisture is the essence of wetness, and wetness is the essence of beauty.
+        Likbay est votre nouveau partnenaire de shopping avec livraison à domicile.
       </p>
       <div className="d-flex align-items-center justify-content-center mt-3 mb-5">
         <Link href="/about">
           <a className="d-flex py-3 align-items-center font-color-black borderbottom border-color-black">
-            <p className="mr-3">Find out more</p>
+            <p className="mr-3">En savoir plus</p>
             <img src="/icon/arrow-long-right.svg" />
           </a>
         </Link>
